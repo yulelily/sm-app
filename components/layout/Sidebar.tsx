47497fb0ad962b1872851569713004cff29db5f9
yulelily <124838371+yulelily@users.noreply.github.com>
2023-12-09 18:51:13 -0500

    Add: sidebar icons

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { BsHouseFill, BsBellFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
   const items = [
@@ -20,7 +22,23 @@ const Sidebar = () => {
     },
   ];
 
-  return <div></div>;
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            /> 
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
