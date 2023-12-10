37d2bbf41aa8372a9ec3a7b91382be5ed4afc57c
yulelily <124838371+yulelily@users.noreply.github.com>
2023-12-09 19:25:25 -0500

    Add: home screen header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import Header from "@/components/layout/Header";
+
 export default function Home() {
   return (
-    <div className="text-3xl font-bold underline text-white">Hello World!</div>
+    <>
+      <Header showBackArrow label="Home" />
+    </>
   );
 }
