b59e95a94b7dd6987f33d4165955a55340be3661
yulelily <124838371+yulelily@users.noreply.github.com>
2023-12-11 18:16:03 -0500

    Add: use like hooks

---
+++ hooks/useLike.ts
@@ -0,0 +1,59 @@
+import { useCallback, useMemo } from "react";
+import useCurrentUser from "./useCurrentUser";
+import useLoginModal from "./useLoginModal";
+import usePost from "./usePost";
+import usePosts from "./usePosts";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+const useLike = ({ postId, userId }: { postId: string; userId?: string }) => {
+  const { data: currentUser } = useCurrentUser();
+  const { data: fetchedPost, mutate: mutateFetchedPost } = usePost(postId);
+  const { mutate: mutateFetchedPosts } = usePosts(userId);
+  const loginModal = useLoginModal();
+
+  const hasLiked = useMemo(() => {
+    const list = fetchedPost?.likedIds || [];
+
+    return list.includes(currentUser?.id);
+  }, [currentUser?.id, fetchedPost?.likedIds]);
+
+  const toggleLike = useCallback(async () => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    try {
+      let request;
+
+      if (hasLiked) {
+        // unlike
+        request = () => axios.delete("/api/like", { data: { postId } });
+      } else {
+        // like
+        request = () => axios.post("/api/like", { postId });
+      }
+
+      await request();
+      // to refresh the page of posts
+      mutateFetchedPosts();
+      // to refresh the post itself
+      mutateFetchedPost();
+
+      toast.success("Successfully liked!");
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  }, [
+    currentUser,
+    hasLiked,
+    postId,
+    mutateFetchedPost,
+    mutateFetchedPosts,
+    loginModal,
+  ]);
+
+  return { hasLiked, toggleLike };
+};
+
+export default useLike;
