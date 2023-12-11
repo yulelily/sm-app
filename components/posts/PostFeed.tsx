1f7df84bc6659a2e2cdbc4b8b2b7ad9f5d65c412
yulelily <124838371+yulelily@users.noreply.github.com>
2023-12-11 11:03:29 -0500

    Add: post feed and post components

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,20 @@
+import usePosts from "@/hooks/usePosts";
+import PostItem from "./PostItem";
+
+interface PostFeedProps {
+  userId?: string;
+}
+
+const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+  const { data: posts = [] } = usePosts();
+
+  return (
+    <>
+      {posts.map((post: Record<string, any>) => (
+        <PostItem key={post.id} userId={userId} data={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostFeed;
