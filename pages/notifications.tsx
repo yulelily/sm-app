bbca53e38801dec3b16c72446bbef8b9f7212067
yulelily <124838371+yulelily@users.noreply.github.com>
2023-12-12 00:07:06 -0500

    Fix: notifications button
    1. fix notifications button not redirecting to notifications page

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,26 +1,5 @@
 import NotificationsFeed from "@/components/NotificationsFeed";
 import Header from "@/components/layout/Header";
-import useCurrentUser from "@/hooks/useCurrentUser";
-import { NextPageContext } from "next";
-import { getSession } from "next-auth/react";
-
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      session,
-    },
-  };
-}
 
 export default function Notifications() {
   return (
